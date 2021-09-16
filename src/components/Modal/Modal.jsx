bb123221814cb5e73c522e82347a1348c28c7079
brenno-calado <brenno.calado@gmail.com>
2021-09-16 12:32:43 -0300

    create Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Modal.css';
 
-export const Modal = () => {
+export const Modal = (props) => {
+  const { toggle } = props;
   return (
-    <aside>
-      <p>Modal</p>
-    </aside>
+    <>
+      <aside onClick={() => toggle()} />
+      <form>
+        <label htmlFor="name">
+          Name:
+          <input id="name" type="text" minLength="6" required autoFocus />
+        </label>
+          BirthDay:
+        <label htmlFor="date">
+          <input type="date" id="date" required />
+        </label>
+        <label htmlFor="gender">
+          Gender
+          <select id="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+        <label htmlFor="cpf">
+          CPF
+          <input id="cpf" type="text" maxLength="11" required />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input type="email" required />
+        </label>
+        <label htmlFor="startDate">
+          Start Date
+          <input id="startDate" type="date" required />
+        </label>
+        <label htmlFor="team">
+          Team
+          <input id="team" type="text" />
+        </label>
+        <button>Create</button>
+      </form>
+    </>
   );
+};
+
+Modal.propTypes = {
+  toggle: PropTypes.func,
 };
